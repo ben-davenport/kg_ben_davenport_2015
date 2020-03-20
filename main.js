710a6f31fc1b35b32cbb860fe792bbaf457a4539
Ben Davenport <benjamin.p.davenport@gmail.com>
2020-03-20 13:28:57 -0400

    improved and optimized solution

--- main.js
+++ main.js
@@ -20,50 +20,33 @@
 // second array.
 // returns false if mapping is inconsistent. 
 
-//feels like it could be simplified from nested for loops
-
-function mappingCheck(array1, array2){  
-    //this for loop iterates through array1    
-    for(let ind = 0; ind < array1.length ; ind++){
-        let indexArr=[];
-        indexArr.push(ind);
-        // this for loop checks for duplicates and pushes them to
-        // indexArr
-        for(let repeat=ind+1; repeat<array1.length; repeat++){
-            if(array1[repeat] === array1[ind]){
-                indexArr.push(repeat)
-            }
-        }
-        // if indexArr has duplicates, it checks the corresponding
-        // mapped values in array2. if they're different, return false.
-        if(indexArr.length > 1){
-            for(let check= 0; check < indexArr.length-1; check++){
-                if(array2[indexArr[check]] != array2[indexArr[check+1]]){
-                    console.log('FALSE');
-                    return false;
-                }
-            }
-        }
-    }
-    console.log('true')
-    return true;
-}
-
-// takes two strings, standardizes them, splits into arrays
-// calls mappingCheck to see if the character mapping is correct;
-function kargo (s1, s2){
-    const arr1 = args1.toLowerCase().split('');
-    const arr2 = args2.toLowerCase().split('');
-    if (arr1.length !== arr2.length){
-        console.log('false')
-        return false
-    }
-    else{
-        mappingCheck(arr1, arr2);
-    }
-}
-
 //passing in command line arguments
 const args1 = process.argv[2];
 const args2 = process.argv[3]
-kargo(args1, args2);
+
+
+function mappingIt (s1, s2){
+    const mappedChars = {};
+
+    if(s1.length !== s2.length){
+        console.log('false')
+        return false;
+    }
+    else{
+        for(let x=0; x<s1.length; x++){
+            let newKey = s1[x];
+            let newValue = s2[x];
+
+            if (!mappedChars[newKey]){
+                mappedChars[newKey] = newValue;
+            }
+            else if (mappedChars[newKey] !== newValue){
+                console.log("false");
+                return false;
+            }
+        };
+    console.log('true')
+    return true
+    };
+}
+mappingIt(args1, args2)
