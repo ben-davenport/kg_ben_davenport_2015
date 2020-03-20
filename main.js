2350e6b91e1e30c6e23c89d87e8ceab8e0e46bca
Ben Davenport <benjamin.p.davenport@gmail.com>
2020-03-20 16:55:17 -0400

    updated comments

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
 // Question:
 // Determine whether a one-to-one character mapping exists from 
 // one string, s1, to another string, s2.
-// For example, given s1 = abc and s2 = bcd,
-
-// print "true" into stdout since we can map each
-// character in "abc" to a character in "bcd".
-
-// Given s1 = foo and s2 = bar, print "false" 
-// since the character ‘o’ cannot map to two characters.
-
-// But given s1 = bar and s2 = foo, print "true" 
-// since each character in "bar" can be mapped to a
-// character in "foo".
-
 // -------------------------------------------------
 
-// takes the indeces of repeating characters in the first array
-// and checks if the mapping of that character is consistent in the
-// second array.
-// returns false if mapping is inconsistent. 
 
-//passing in command line arguments
+// passing in command line arguments
 const args1 = process.argv[2];
-const args2 = process.argv[3]
+const args2 = process.argv[3];
 
+// map s1 characters as keys
+// map corresponding s2 characters as values
+// if an s1 character repeats
+// check if associated s2 value is consistent
 
 function mappingIt (s1, s2){
     const mappedChars = {};
 
     if(s1.length !== s2.length){
-        console.log('false')
+        console.log('false');
         return false;
     }
     else{
@@ -44,9 +32,10 @@ function mappingIt (s1, s2){
             console.log("false");
             return false;
         }
-    }
-    console.log('true')
+    };
+    console.log('true');
     return true
 };
-}
+};
+
 mappingIt(args1, args2)
